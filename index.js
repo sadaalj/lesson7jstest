e75837fbf06f18c55a3177724de124692c7a920f
Sada.Aljondi <sadaaljondi@hotmail.com>
2025-06-29 13:19:40 -0400

    testing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
     const jsNum1 = core.getInput('number1');
     const jsNum2 = core.getInput('number2');
     result = parseInt(jsNum1) + parseInt(jsNum2);
-    core.setOutput('sum', result);
+    core.setOutput("result", result);
 }catch(error){
-    core.setFailed(`Action failed with error: ${error.message}`);
+    core.setFailed(error.message);
 }
